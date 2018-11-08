0dc51e3c4300af2bf7bf5dd1c6ae6f136cecab63
andrew <Muelleau1917@gmail.com>
2018-11-08 09:44:58 -0500

    adding point class

---
+++ src/point/point.hpp
@@ -0,0 +1,29 @@
+
+#ifndef point_H
+#define point_H
+
+#include <string>
+#include <map>
+
+
+class Point {
+
+    public:
+
+        std::string code;
+        std::map<std::string, double> outlets;
+        double dollars;
+        double x{0}, y{0};
+        double cluster = 0;
+
+        void addOutlet(std::string name_, double dollars_) {
+            if ( outlets.find(name_) == outlets.end() ) {
+                outlets.insert({name_, dollars_});
+            } 
+            outlets.at(name_) += dollars_;
+            dollars += dollars_;
+        };
+
+};
+
+#endif
